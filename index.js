e07ed7320ae208f4fe579a91b0addcaa517c9ae6
Josh Yap <joshua.c.yap@gmail.com>
2022-02-09 19:46:02 -0600

    static test

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 const url = 'https://www.ercot.com/content/cdr/html/real_time_system_conditions.html';
 app.use(cors());
-app.use(express.static(__dirname + '/src'))
-
+// app.use(express.static(__dirname + '/src'))
+app.use(express.static('src'));
 // const path = require('path');
 
 
